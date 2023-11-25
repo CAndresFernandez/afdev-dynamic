dfb0b478b1169f078ff232fb64d230838aae31b4
Andres Fernandez <andres-carlos.fernandez-baker@oclock.school>
2023-11-25 16:46:27 +0100

    fixed edit/new form bug

--- src/Form/ExperienceType.php
+++ src/Form/ExperienceType.php
@@ -7,6 +7,7 @@ use Symfony\Component\DomCrawler\Field\TextareaFormField;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
@@ -17,11 +18,11 @@ class ExperienceType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('company')
-            ->add('startYear')
-            ->add('endYear')
-            ->add('location')
-            ->add('jobTitle')
+            ->add('company', TextType::class)
+            ->add('startYear', NumberType::class)
+            ->add('endYear', NumberType::class)
+            ->add('location', TextType::class)
+            ->add('jobTitle', TextType::class)
             ->add('icon', ChoiceType::class, [
                 'choices' => [
                     'School' => 1,
